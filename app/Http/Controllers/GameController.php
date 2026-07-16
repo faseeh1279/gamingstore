9d079cd7d7d43692f9d413166b72718024fad8d8
faseeh1279 <faseehraza1279@gmail.com>
2026-07-16 15:12:34 +0500

    Added GameControllerPage

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -6,31 +6,27 @@ use App\Models\Game;
 use App\Services\GameService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests; 
 
 class GameController extends Controller
 {
+    use AuthorizesRequests;
     protected GameService $gameService; 
 
     public function __construct(GameService $gameService){ 
         $this->gameService = $gameService; 
     }
 
+    public function create(){ 
+        $this->authorize('create', Game::class); 
+        return view('admin.games.create');
+    }
+
     public function index(){ 
-        $games = $this->gameService->fetchGames(); 
-        $user = Auth::user(); 
-        return view('games.index', compact('user')); 
-    }   
-
-    public function createGame(){ 
-        $genres = $this->gameService->fetchGenres(); 
-        return view ('games.create', compact('genres')); 
+        return view('admin.games.index');
     }
-
-    public function store(Request $request){ 
-        $this->gameService->createGame($request); 
-        return redirect()->route('games.index')->with(201, 'Game created successfully.'); 
-    }
-
     
+    public function dashboard(){ 
+        return view('admin.dashboard.index'); 
+    }
 }
